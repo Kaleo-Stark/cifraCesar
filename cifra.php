0a32108aaddec20a8bf74a5462ba8554a717f51b
kaleoStark <kaleostark@eldrazi>
2020-06-18 10:11:14 -0300

    palavras para letras

--- cifra.php
+++ cifra.php
@@ -1,5 +1,5 @@
 <?php
-// ARRAY COM AS PALAVRAS DO ALFABETO 
+// ARRAY COM AS LETRAS DO ALFABETO 
 $letras = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ');
 
 //###############( RECEBE OS DADOS POSTADOS )################
